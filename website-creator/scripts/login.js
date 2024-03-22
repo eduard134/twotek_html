56a745788ca1f6581db256833f58d31b4f3cf6b8
Loghin Dan <loghindan494@gmail.com>
2024-03-22 20:11:57 +0200

    update

---
+++ website-creator/scripts/login.js
@@ -0,0 +1,10 @@
+
+
+function credentials(){
+let firstName = document.getElementById('firstName').value
+let lastName= document.getElementById('lastName').value
+let mail = document.getElementById('mail').value
+
+console.log(firstName, lastName, mail)
+
+}
