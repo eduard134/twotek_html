ef76c0f4ad4d99b43661521dfdadb02cc0ff4333
Loghin Dan <loghindan494@gmail.com>
2024-03-24 23:04:54 +0200

    update

--- website-creator/scripts/server.js
+++ website-creator/scripts/server.js
@@ -3,32 +3,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();  
 const bodyParser=require('body-parser')
-const port = 3000;
 
 app.use(bodyParser.urlencoded({extended:true}))
 
 const LogInSchema={
-    name: fullName,
-    mail: email
+    name: String,
+    mail: String
 }
 
 const credentials= mongoose.model("Credentials", LogInSchema)
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/login.html')
+    res.sendFile(__dirname + '/website-creator/pages/login.html')
 })
 
-app.post('/', function(req, res){
-    let newCredentials= new Credential({
+app.post('', function(req, res){
+    let newCredentials= new credentials({
         name: req.body.name,
         mail: req.body.mail
     })
     newCredentials.save()
-    res.redirect('/')
-})
-
-app.listen(3000, function(){
-    console.log("server is running on 3000")
+    res.redirect('/website-creator/pages/login.html')
 })
 
 mongoose.connect('mongodb+srv://loghindan494:NewPass2024!@twotek.bvnhes9.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true })
